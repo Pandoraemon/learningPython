45ba34edd34734401a488be3ee203580623ad3b0
pandoraemon <pz880304@163.com>
2016-01-16 09:24:56 +0800

    delete old direct  Please enter the commit message for your changes. Lines starting

--- Lpthw/ex13.py
+++
@@ -1,9 +0,0 @@
-#-- coding: utf-8 --
-from sys import argv
-
-script, first, second, third, fourth = argv
-
-print "The scirpt is called: ",script
-print "Your first variable is: ",first
-print "Your second variable is: ",second
-print "Your thrid variable is: ", third
