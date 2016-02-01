7f0870b003acf7f3b5086499bf70ae1d3aa0ddea
pandoraemon <pz880304@163.com>
2016-02-01 15:42:35 +0800

    add Post view

--- 3_Myweibo/app/main/views.py
+++ 3_Myweibo/app/main/views.py
@@ -1,15 +1,22 @@
 from flask import render_template, session, redirect, url_for, current_app, abort, flash
 from . import main
-from .form import EditProfileForm, EditProfileAdminForm
+from .form import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
-from ..models import User, Role
+from ..models import User, Role, Permission, Post
 from flask.ext.login import login_required, current_user
 from ..decorators import admin_required
 
 
-@main.route('/',)
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
